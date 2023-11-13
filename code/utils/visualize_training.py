c188341f84b17c06d3869b8d183d2e87e3ff23a1
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2023-11-13 19:16:44 -0300

    Updated debug visualization code

--- code/utils/visualize_training.py
+++ code/utils/visualize_training.py
@@ -9,22 +9,32 @@ import gymnasium
 from gym_duckietown.wrappers import DiscreteDifferentialWrapper
 
 
+def next_action(cur_action: int):
+    if cur_action == 2:
+        return 0
+    return cur_action + 1
+
+
 def train(id):
     print(f"Running train on process {id}")
     env = gymnasium.make("Duckietown-udem1-v0_d")
     env = DiscreteDifferentialWrapper(env)
     env.reset(seed=123)
+    # Action is 0, 1, or 2
+    action = 0
+    counter = 0
+
     while True:
-        # Action is 0, 1, or 2
-        action = 2
+        counter += 1
+        if counter % 60 == 0:
+            action = next_action(action)
+
         obs, rew, done, trunc, *_ = env.step(action)
         env.render()
-        if done:
-            print("Done!")
-            env.reset(seed=123)
-            continue
-
-    print(f"Thread {id} done.")
+        # if done:
+        #     print("Done!")
+        #     env.reset(seed=123)
+        #     continue
 
 
 train(1)
