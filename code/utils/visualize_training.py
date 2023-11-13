fc9c6f3a84895cf1b78bf40ae87a34cf9529bb96
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2023-11-13 18:50:09 -0300

    Code cleanup, updated gym module

--- code/utils/visualize_training.py
+++ code/utils/visualize_training.py
@@ -6,26 +6,17 @@ from imgui.integrations.pyglet import create_renderer
 from typing import List
 import gymnasium
 
-from gym_duckietown.wrappers import DiscreteWrapper
-
-from . import gui
-from multiprocessing.connection import Client, Listener
-
-client_addr = ("localhost", 6001)
-server_addr = ("localhost", 6000)
-listener = Listener(server_addr, authkey=b"secret password")
+from gym_duckietown.wrappers import DiscreteDifferentialWrapper
 
 
 def train(id):
     print(f"Running train on process {id}")
-    env = gymnasium.make("Duckietown-udem1-v0")
-    # env = make_vec_env(
-    #     "Duckietown-udem1-v0", n_envs=1, wrapper_class=wrap, seed=SEED + 100 * id
-    # )
-    # env = VecFrameStack(env, 5)
+    env = gymnasium.make("Duckietown-udem1-v0_d")
+    env = DiscreteDifferentialWrapper(env)
     env.reset(seed=123)
     while True:
-        action = [0.3, 0]
+        # Action is 0, 1, or 2
+        action = 2
         obs, rew, done, trunc, *_ = env.step(action)
         env.render()
         if done:
