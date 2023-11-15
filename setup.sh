98dad627f295ac193b6376b3bc5c3dd6031d5af9
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2023-11-14 22:39:00 -0300

    Update setup.sh
    Added missing dependencies

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@
 
 pip install stable-baselines3[extra]
 pip install gymnasium
+pip install tensorboard
+pip install wandb
+pip install pygame
 pip install -e .\gym-duckietown
 pip install -e .\duckietown-world
