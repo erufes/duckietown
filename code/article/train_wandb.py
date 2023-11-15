498c3a7ae9eddcfd4ca66d22841c43d1fdcd3085
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2023-11-14 22:20:04 -0300

    Update train_wandb.py

--- code/article/train_wandb.py
+++ code/article/train_wandb.py
@@ -17,9 +17,12 @@ from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env.vec_frame_stack import VecFrameStack
 from stable_baselines3.common.monitor import Monitor
 from gym_duckietown.custom_net import CustomCNN
+import multiprocessing
+from typing import List
 
-MODEL_PREFIX = "dqn_cnet_wrapped_stack"
-SEED = 2**30 + 8394
+MODEL_PREFIX = "dqn_wandb_cnet_wrapped_stack"
+SEED = 2**30 + 89023
+THREAD_COUNT = 4
 
 
 policy_kwargs = dict(
@@ -28,77 +31,86 @@ policy_kwargs = dict(
 )
 
 
-config = {
-    "policy_type": "CnnPolicy",
-    "total_timesteps": 500_000,
-    "env_id": "Duckietown-udem1-v0_pietroluongo_train",
-    "batch_size": 32,
-    "gamma": 0.99,
-    "learning_rate": 0.00005,
-    "buffer_size": 50000,
-    "learning_starts": 10000,
-    "seed": SEED,
-    "verbose": 2,
-    "optimize_memory_usage": True,
-    "replay_buffer_kwargs": {"handle_timeout_termination": False},
-    "policy_kwargs": policy_kwargs,
-}
+def train(id):
+    config = {
+        "policy_type": "CnnPolicy",
+        "total_timesteps": 500_000,
+        "env_id": "Duckietown-udem1-v0_pietroluongo_train",
+        "batch_size": 32,
+        "gamma": 0.99,
+        "learning_rate": 0.00005,
+        "buffer_size": 50000,
+        "learning_starts": 10000,
+        "seed": SEED // id,
+        "verbose": 2,
+        "optimize_memory_usage": True,
+        "replay_buffer_kwargs": {"handle_timeout_termination": False},
+        "policy_kwargs": policy_kwargs,
+    }
 
-run = wandb.init(
-    project="pgzitos",
-    config=config,
-    sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
-)
+    run = wandb.init(
+        project="pgzitos",
+        config=config,
+        sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+    )
 
+    def wrap(env):
+        env = DiscreteDifferentialWrapper(env)
+        env = segment(env)
+        return env
 
-def wrap(env):
-    env = DiscreteDifferentialWrapper(env)
-    env = segment(env)
-    return env
+    def segment(env):
+        env = CropObservation(env, 300)
+        env = SegmentLaneWrapper(env)
+        env = SegmentMiddleLaneWrapper(env)
+        env = SegmentRemoveExtraInfo(env)
+        env = ResizeObservation(env, (40, 80))
+        env = TransposeToConv2d(env)
+        env = Monitor(env)
+        return env
 
+    env = make_vec_env(
+        "Duckietown-udem1-v0_pietroluongo_train",
+        n_envs=1,
+        wrapper_class=wrap,
+        seed=SEED,
+    )
+    env = VecFrameStack(env, 5)
 
-def segment(env):
-    env = CropObservation(env, 300)
-    env = SegmentLaneWrapper(env)
-    env = SegmentMiddleLaneWrapper(env)
-    env = SegmentRemoveExtraInfo(env)
-    env = ResizeObservation(env, (40, 80))
-    env = TransposeToConv2d(env)
-    env = Monitor(env)
-    return env
-
-
-env = make_vec_env(
-    "Duckietown-udem1-v0_pietroluongo_train",
-    n_envs=1,
-    wrapper_class=wrap,
-    seed=SEED,
-)
-env = VecFrameStack(env, 5)
-
-model = DQN(
-    policy=config["policy_type"],
-    batch_size=config["batch_size"],
-    env=env,
-    gamma=config["gamma"],
-    learning_rate=config["learning_rate"],
-    buffer_size=config["buffer_size"],
-    tensorboard_log="./wandb_runs",
-    learning_starts=config["learning_starts"],
-    seed=config["seed"],
-    verbose=2,
-    optimize_memory_usage=config["optimize_memory_usage"],
-    replay_buffer_kwargs=config["replay_buffer_kwargs"],
-    policy_kwargs=config["policy_kwargs"],
-)
-
-
-model.learn(
-    total_timesteps=config["total_timesteps"],
-    callback=WandbCallback(
-        model_save_path=f"wandb_models/{run.id}",
+    model = DQN(
+        policy=config["policy_type"],
+        batch_size=config["batch_size"],
+        env=env,
+        gamma=config["gamma"],
+        learning_rate=config["learning_rate"],
+        buffer_size=config["buffer_size"],
+        tensorboard_log="./wandb_runs",
+        learning_starts=config["learning_starts"],
+        seed=config["seed"],
         verbose=2,
-    ),
-)
+        optimize_memory_usage=config["optimize_memory_usage"],
+        replay_buffer_kwargs=config["replay_buffer_kwargs"],
+        policy_kwargs=config["policy_kwargs"],
+    )
 
-run.finish()
+    model.learn(
+        total_timesteps=config["total_timesteps"],
+        callback=WandbCallback(
+            model_save_path=f"wandb_models/id/{run.id}",
+            verbose=2,
+        ),
+    )
+
+    run.finish()
+
+
+if __name__ == "__main__":
+    procs: List[multiprocessing.Process] = []
+    for i in range(1, THREAD_COUNT + 1):
+        procs.append(multiprocessing.Process(target=train, args=[i]))
+
+    for p in procs:
+        p.start()
+
+    for p in procs:
+        p.join()
